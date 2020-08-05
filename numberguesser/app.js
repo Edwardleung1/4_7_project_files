3698ad010113eeac95cb8cd33b7b4cf8eace3430
Edward Leung <edward_leung_12@hotmail.com>
2020-08-05 17:55:59 +0100

    updated UI elements

--- numberguesser/app.js
+++ numberguesser/app.js
@@ -14,7 +14,9 @@ let winningNumber = 2;
 let guessesLeft = 3;
 
 // UI Elements
-const game = document.getElementById("game");
+const game = document.querySelector("#game");
 const minNum = document.querySelector(".min-num");
 const maxNum = document.querySelector(".max-num");
 const guessBtn = document.querySelector("#guess-btn");
+const guessInput = document.querySelector("#guess-input");
+const message = document.querySelector(".message");
